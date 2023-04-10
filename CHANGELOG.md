e04f6e5811ee35e26ae4ffaf4c294a4aa9273867
Johannes Schaffer <johannes.schaffer.gs@gmail.com>
2023-04-10 14:44:57 +0200

    feat!: change findHeadings() api & refine implementation & doc it

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,8 @@
 # CHANGELOG
 
 ## v3.0.0
--
-- feat!: add documentation, change findHeadings() api
+- doc: add serialize() documentation
+- feat!: change findHeadings() api & refine implementation & doc it
 
 ## v2.0.0
 - feat!: custom lexical serializer instead of using @lexical/headless, added README, added CHANGELOG, renamed files to camelcase
--- src/utils/findHeadings.ts
+++ src/utils/findHeadings.ts
@@ -1,5 +1,6 @@
-import {SerializedEditorState, SerializedTextNode} from "lexical";
-import {SerializedHeadingNode} from "@lexical/rich-text";
+import type {SerializedTextNode, SerializedRootNode} from "lexical";
+import type {SerializedHeadingNode} from "@lexical/rich-text";
+import {isHeadingNode} from "../helpers/nodeGuards";
 
 const getHeadingText = (heading: SerializedHeadingNode) => (
     // Multiple children when headings has multiple lines (Shift + Enter)
@@ -7,11 +8,14 @@ const getHeadingText = (heading: SerializedHeadingNode) => (
         .reduce((prev, curr) => prev + curr.text, '')
 )
 
-export const findHeadings = (state: string, tag: SerializedHeadingNode['tag'] = 'h1') => {
-    const serialized: SerializedEditorState = JSON.parse(state)
-    return serialized.root.children.map(child => {
-        if (child.type == 'heading' && (child as SerializedHeadingNode).tag == tag) {
-            return getHeadingText(child as SerializedHeadingNode)
-        }
-    }).filter(entry => !!entry)
-}
+/**
+ * @return List of all heading texts, matching the specified tag*/
+export const findHeadings = (root: SerializedRootNode, tag: SerializedHeadingNode['tag'] = 'h1') => (
+    root.children
+        .map(node =>
+            isHeadingNode(node) &&
+            node.tag == tag &&
+            getHeadingText(node)
+        )
+        .filter(entry => !!entry)
+)
